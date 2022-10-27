8060a7bf8eae084e0a324581dc3a1efe8359b709
keith-mar <keith.mar@govx.com>
2022-10-27 16:25:39 -0700

    memberspace

--- sdtc/pages/_app.js
+++ sdtc/pages/_app.js
@@ -1,8 +1,19 @@
 import "../styles/globals.css";
 import NavFrame from "../components/nav/NavFrame";
+import Script from "next/script";
 function MyApp({ Component, pageProps }) {
   return (
     <div>
+      <Script id="test" strategy="beforeInteractive">
+        {`console.log('hi');var MemberSpace = window.MemberSpace || {"subdomain":"onrender"};
+(function(d){
+  var s = d.createElement("script");
+  s.src = "https://cdn.memberspace.com/scripts/widgets.js";
+  var e = d.getElementsByTagName("script")[0];
+  e.parentNode.insertBefore(s,e);
+}(document));
+      `}
+      </Script>
       <NavFrame>
         <Component {...pageProps} />
       </NavFrame>
